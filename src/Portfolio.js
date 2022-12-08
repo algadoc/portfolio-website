5e310e03759d2912acf137bd95c8f6fffe47c6df
Alfonso Lagares de Toledo <alagares@gatech.edu>
2022-12-08 08:25:30 -0500

    Add existing files to repo

---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tabs from "./components/Tabs";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <p>Welcome to my portfolio website!</p>
+      <Tabs />
+      ...
+    </div>
+  );
+};
+
+export default Portfolio;
